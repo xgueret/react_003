41dd3e8152f40de7b27393cbbbfa48c361f24c32
xgueret <971xavier.gueret@gmail.com>
2022-03-04 22:04:03 +0100

    hero begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import styledComponents from 'styled-components'
-/*import Hero from './components/Hero'*/
+import travel_01 from './asset/travel1.jpg'
+//import travel_02 from './assets/travel-02.jpg'
+//import travel_03 from './assets/travel-03.jpg'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 /*import Slider from './components/Slider'*/
 
@@ -9,8 +12,8 @@ const App = () => {
   return (
     <Container>
       <Navbar />
-      {/*<Hero />
-      <Slider />*/}
+      <Hero imageSrc={travel_01} />
+      {/*<Slider />*/}
     </Container>
   )
 }
