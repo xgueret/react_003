801c74823141201a752a2ecbc43c9513b117d1d1
xgueret <971xavier.gueret@gmail.com>
2022-03-04 22:04:12 +0100

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Hero = () => {
-  return <div>Hero</div>
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  position: absolute;
+`
+const Title = styled.h1`
+  position: absolute;
+  bottom: 15%;
+  right: 10%;
+  color: white;
+  font-size: 4rem;
+  padding: 15px;
+`
+
+const Hero = ({ imageSrc }) => {
+  return (
+    <Container>
+      <Image src={imageSrc} />
+      <Title>Travel made simple.</Title>
+    </Container>
+  )
 }
 
 export default Hero
